c5ca66ae4955628022b79db8b2ca9dea5a739921
KwonTaeYang <35719387+sunnight9507@users.noreply.github.com>
2020-06-27 14:49:31 +0900

    Add files via upload

--- 1-2주차/R기초 교육.R
+++ 1-2주차/R기초 교육.R
@@ -1,1 +1,33 @@
-# 1. intro
+##### 1. introduction
+
+### 1) The age of data
+## 데이터의 시대
+# - 데이터의 폭발적인 증가
+# - 데이터를 잘 다룰 줄 아는 기업, 개인이 성공하는 시대가 도래
+
+### 2) Data
+## 정의
+# 추정과 추론의 근거가 되는 사실(옥스포드 사전)
+# 어떤 주제로 모아진 값들의 집합
+
+## 형태
+# 정형 / 비정형
+
+### 3) 빅데이터
+# 기존 데이터 베이스 관리도구의 데이터 수집, 저장, 관리, 분석 역량을 넘어서는 데이터
+
+## 3V
+# Volume(크기) / Variety(다양성) / Velocity(속도)
+
+### 4) 데이터 분석 절차
+# Problen Definition
+# Data Management
+# Exploration
+# Analysis & Interpretation
+# Reporting
+
+## 분석 도구
+# SAS / SPSS / Matlab / R / Python 
+
+### 5) Intall R, Rstudio
+# 실행 단축키 : Ctrl + Enter
